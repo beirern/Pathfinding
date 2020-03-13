17dea8f54d5665097a6b76c10f93417be83e86f0
Nicola Beirer <beirern@uw.edu>
2020-03-12 17:28:42 -0700

    Able to draw and delete walls

--- board/canvas/canvas.py
+++ board/canvas/canvas.py
@@ -12,30 +12,56 @@ class Canvas(tk.Canvas):
         super().__init__(master=master, width=width, height=height)
         self.pack()
 
-        self.rect = None
+        self.currRect = None
 
         self.width = width
         self.height = height
 
         self.walls = []
 
-        self.bind("<B1-Motion>", self.draw)
-        self.bind("<Button-1>", self.draw)
+        # Left Click for making Walls
+        self.bind("<Button-1>", self.buttonOneClick)
+        self.bind("<B1-Motion>", self.buttonOneMotion)
+        self.bind("<ButtonRelease-1>", self.buttonOneRelease)
 
-        # self.array = np.empty((height, width), np.int)
-        # print(self.array)
+        # Right Click Deletes Walls
+        self.bind("<Button-3>", self.clearLast)
+        self.bind("<Double-Button-3>", self.clearAll)
 
-    def draw(self, event):
-        if event.state == 0:
-            self.start_x = event.x
-            self.start_y = event.y
-        elif event.state == 256:
-            self.end_x = event.x
-            self.end_y = event.y
-            self.create_rectangle(self.start_x, self.start_y,
-                                  self.end_x, self.end_y, fill="black")
-            self.walls.append(
-                [self.start_x, self.start_y, self.end_x, self.end_y])
+    # Get start x and y on Click
+    def buttonOneClick(self, event):
+        self.currRect = None
+        self.start_x = event.x
+        self.start_y = event.y
 
-    def test2(self, event):
-        print('Hi:', event.x)
+    # Draw Rectangle as curser is moving
+
+    def buttonOneMotion(self, event):
+        if (self.currRect):
+            self.delete(self.currRect)
+        self.end_x = event.x
+        self.end_y = event.y
+        self.currRect = self.create_rectangle(self.start_x, self.start_y,
+                                              self.end_x, self.end_y, width=1, fill="blue")
+
+    # Draw final wall and save it on Release
+    def buttonOneRelease(self, event):
+        if (self.currRect):
+            self.delete(self.currRect)
+        self.end_x = event.x
+        self.end_y = event.y
+        self.currRect = self.create_rectangle(self.start_x, self.start_y,
+                                              self.end_x, self.end_y, width=1, fill="blue")
+        self.walls.append(self.currRect)
+
+    # Remove last Drawn Wall
+    def clearLast(self, event):
+        if (len(self.walls) > 0):
+            self.delete(self.walls[-1])
+            del self.walls[-1]
+
+        # Removes all Walls
+    def clearAll(self, event):
+        if (len(self.walls) > 0):
+            self.delete(tk.ALL)
+            self.walls = []
