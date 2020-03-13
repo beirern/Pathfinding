d8cd5a81b0f86616a861868daaae3def1db81e1a
Nicola Beirer <beirern@uw.edu>
2020-03-13 14:41:37 -0700

    added isWall field

--- board/canvas/pixel/pixel.py
+++ board/canvas/pixel/pixel.py
@@ -1,7 +1,8 @@
 class Pixel():
-    def __init__(self, x, y):
+    def __init__(self, x, y, isWall):
         self.x = x
         self.y = y
+        self.isWall = isWall
 
     def __str__(self):
-        return "x: " + x + ", y: " + y
+        return "x: " + x + ", y: " + y + ", Wall: " + self.isWall
