571525d0830fe1b84487137019d9a891c9cb02f8
Nicola Beirer <beirern@uw.edu>
2020-03-14 17:45:04 -0700

    Added pixel array logic and import aStarGraph

--- board/canvas/canvas.py
+++ board/canvas/canvas.py
@@ -3,6 +3,8 @@ import numpy as np
 from .enemy import Enemy
 from .player import Player
 from .shape import Shape
+from .pathfinding import AstarGraph
+from .pathfinding import Pixel
 
 
 class Canvas(tk.Canvas):
@@ -17,6 +19,14 @@ class Canvas(tk.Canvas):
 
         self.walls = []
 
+        # Set up array of Pixels
+        self.pixels = []
+        for i in range(self.height):
+            self.pixels.append([])
+            for j in range(self.width):
+                self.pixels[-1].append(Pixel(j,
+                                             i, i * len(self.pixels[0]) + j, True))
+
         # Left Click for making Walls
         self.bind("<Button-1>", self.buttonOneClick)
         self.bind("<B1-Motion>", self.buttonOneMotion)
@@ -26,9 +36,12 @@ class Canvas(tk.Canvas):
         self.bind("<Button-3>", self.clearLast)
         self.bind("<Double-Button-3>", self.clearAll)
 
-        self.player = Player(Shape(800, 400, 825, 425, "Rectangle"))
-        self.enemy = Enemy(Shape(50, 50, 75, 75, "Rectangle"))
+        self.player = Player(Shape(800, 400, 824, 424, "Rectangle"))
+        self.enemy = Enemy(Shape(50, 50, 74, 74, "Rectangle"))
         self.drawPlayers()
+
+        # Enter to Start Game
+        self.bind("<Tab>", self.startGame)
 
     # Get start x and y on Click
     def buttonOneClick(self, event):
@@ -59,6 +72,10 @@ class Canvas(tk.Canvas):
                                                   self.currRect_end_x, self.currRect_end_y, width=1, fill="blue")
             self.walls.append(Shape(self.currRect_start_x, self.currRect_start_y,
                                     self.currRect_end_x, self.currRect_end_y, "Rectangle", self.currRect))
+
+            for j in range(self.walls[-1].x1, self.walls[-1].x2 + 1):
+                for i in range(self.walls[-1].y1, self.walls[-1].y2 + 1):
+                    self.pixels[i][j].is_movable_to = False
 
     # Helper Method to check if a wall will cover up a player
     def validWall(self):
@@ -128,14 +145,29 @@ class Canvas(tk.Canvas):
         playerShape = self.player.shape
 
         if playerShape.shapeType == "Rectangle":
+            # Draw Player
             canvas_id = self.create_rectangle(
                 playerShape.x1, playerShape.y1, playerShape.x2, playerShape.y2, fill="green")
             self.player = Player(
                 Shape(playerShape.x1, playerShape.y1, playerShape.x2, playerShape.y2, "Rectangle", canvas_id))
+            # Update Pixel Array
+            for j in range(self.player.shape.x1, self.player.shape.x2 + 1):
+                for i in range(self.player.shape.y1, self.player.shape.y2 + 1):
+                    self.pixels[i][j].is_movable_to = False
 
         enemyShape = self.enemy.shape
         if enemyShape.shapeType == "Rectangle":
+            # Draw Enemy
             canvas_id = self.create_rectangle(
                 enemyShape.x1, enemyShape.y1, enemyShape.x2, enemyShape.y2, fill="red")
             self.enemy = Enemy(
                 Shape(enemyShape.x1, enemyShape.y1, enemyShape.x2, enemyShape.y2, "Rectangle", canvas_id))
+            # Update Pixel Array
+            for j in range(self.enemy.shape.x1, self.enemy.shape.x2 + 1):
+                for i in range(self.enemy.shape.y1, self.enemy.shape.y2 + 1):
+                    self.pixels[i][j].is_movable_to = False
+
+    def startGame(self, event):
+        graph = AstarGraph(self.pixels, self.enemy)
+
+        graph.neighbors(self.pixels[62][62])
