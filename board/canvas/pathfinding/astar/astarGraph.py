32886de6116ed9bc32a9c3e399cb59d200054e9e
Nicola Beirer <beirern@uw.edu>
2020-03-27 20:35:18 -0700

    Changed astar graph for new edges method

--- board/canvas/pathfinding/astar/astarGraph.py
+++ board/canvas/pathfinding/astar/astarGraph.py
@@ -1,3 +1,4 @@
+import math
 from .weightedEdge import WeightedEdge
 from .pixel import Pixel
 
@@ -6,11 +7,10 @@ from .pixel import Pixel
 class AstarGraph:
     # Takes in [][] of all pixels (The grid)
     # A reference to enemy to get height and width to determine valid neighbors
-    def __init__(self, pixels, enemy):
+    def __init__(self, pixels, waypoints):
         self.pixels = pixels
-        self.neighbor_height = enemy.shape.height
-        self.neighbor_width = enemy.shape.width
-        self.edges = []
+        self.waypoints = waypoints
+        self.edges = {}
 
         self.fillEdges()
 
@@ -19,108 +19,18 @@ class AstarGraph:
     # i.e. [UP, RIGHT, DOWN, LEFT]
     # (0, 2) -> [WE(0, 1), WE(1,2), WE(0, 3), None]
     def fillEdges(self):
-        for i in range(len(self.pixels)):
-            for j in range(len(self.pixels[0])):
-                self.edges.append([])
-                # UP
-                if i != 0:
-                    if self.pixels[i-1][j].is_movable_to:
-                        self.edges[-1].append(WeightedEdge(self.pixels[i]
-                                                           [j], self.pixels[i-1][j], 1))
-                    else:
-                        self.edges[-1].append(None)
-                else:
-                    self.edges[-1].append(None)
-                # RIGHT
-                if j != len(self.pixels[0]) - 1:
-                    if self.pixels[i][j+1].is_movable_to:
-                        self.edges[-1].append(WeightedEdge(self.pixels[i]
-                                                           [j], self.pixels[i][j+1], 1))
-                    else:
-                        self.edges[-1].append(None)
-                else:
-                    self.edges[-1].append(None)
-                # DOWN
-                if i != len(self.pixels) - 1:
-                    if self.pixels[i+1][j].is_movable_to:
-                        self.edges[-1].append(WeightedEdge(self.pixels[i]
-                                                           [j], self.pixels[i+1][j], 1))
-                    else:
-                        self.edges[-1].append(None)
-                else:
-                    self.edges[-1].append(None)
-                # LEFT
-                if j != 0:
-                    if self.pixels[i][j-1].is_movable_to:
-                        self.edges[-1].append(WeightedEdge(self.pixels[i]
-                                                           [j], self.pixels[i][j-1], 1))
-                    else:
-                        self.edges[-1].append(None)
-                else:
-                    self.edges[-1].append(None)
+        for waypoint in self.waypoints:
+            self.edges[self.pixels[waypoint.y1][waypoint.x1]] = []
+            for end_point in self.waypoints[waypoint]:
+                self.edges[self.pixels[waypoint.y1][waypoint.x1]].append(WeightedEdge(self.pixels[waypoint.y1][waypoint.x1], self.pixels[end_point.y1][end_point.x1],
+                                                                                      math.sqrt(math.pow(waypoint.y1 - end_point.y1, 2) + math.pow(waypoint.y2 - end_point.y2, 2))))
 
     # Returns valid neighbors
     # Returns the middle pixel in either direction of a valid direction
     # i.e. if enemy height = 5, would return the 3rd pixel left or right
 
     def neighbors(self, pixel):
-        valid_neighbors = []
-        half_height = int(self.neighbor_height / 2)
-        half_width = int(self.neighbor_width / 2)
-
-        # TOP is index 0 for list of edges
-        top = self.pixels[pixel.y - half_height][pixel.x]
-        if top.y != 0:
-            valid_spot = True
-            for j in range(top.x - half_width, top.x + half_width + 1):
-                if self.edges[self.pixels[top.y][j].index][0] == None:
-                    valid_spot = False
-
-            if valid_spot:
-                valid_edge = WeightedEdge(pixel, Pixel(
-                    pixel.x, pixel.y - 1, pixel.y * len(self.pixels[0]) + pixel.x, True), 1)
-                valid_neighbors.append(valid_edge)
-
-        # RIGHT is index 1 for list of edges
-        right = self.pixels[pixel.y][pixel.x + half_width]
-        if right.x != len(self.pixels[0]):
-            valid_spot = True
-            for i in range(right.y - half_height, right.y + half_height + 1):
-                if self.edges[self.pixels[i][right.x].index][1] == None:
-                    valid_spot = False
-
-            if valid_spot:
-                valid_edge = WeightedEdge(pixel, Pixel(
-                    pixel.x + 1, pixel.y, pixel.y * len(self.pixels[0]) + pixel.x, True), 1)
-                valid_neighbors.append(valid_edge)
-
-        # BOTTOM is index 2 for list of edges
-        bottom = self.pixels[pixel.y + half_height][pixel.x]
-        if bottom.y != len(self.pixels):
-            valid_spot = True
-            for j in range(bottom.x - half_width, bottom.x + half_width + 1):
-                if self.edges[self.pixels[bottom.y][j].index][2] == None:
-                    valid_spot = False
-
-            if valid_spot:
-                valid_edge = WeightedEdge(pixel, Pixel(
-                    pixel.x, pixel.y + 1, pixel.y * len(self.pixels[0]) + pixel.x, True), 1)
-                valid_neighbors.append(valid_edge)
-
-        # LEFT is index 3 for list of edges
-        left = self.pixels[pixel.y][pixel.x - half_width]
-        if left.x != 0:
-            valid_spot = True
-            for i in range(left.y - half_height, left.y + half_height + 1):
-                if self.edges[self.pixels[i][left.x].index][3] == None:
-                    valid_spot = False
-
-            if valid_spot:
-                valid_edge = WeightedEdge(pixel, Pixel(
-                    pixel.x - 1, pixel.y, pixel.y * len(self.pixels[0]) + pixel.x, True), 1)
-                valid_neighbors.append(valid_edge)
-
-        return valid_neighbors
+        return self.edges[pixel]
 
     # Returns the manhatten distance squared, heuristic function
     def estimated_distance_to_goal(self, pixel, goal):
