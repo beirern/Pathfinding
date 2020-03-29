77293bff2e616298060fdc0cfb0900e396b19206
Nicola Beirer <beirern@uw.edu>
2020-03-28 19:10:22 -0700

    changed imports

--- board/canvas/functions/__init__.py
+++ board/canvas/functions/__init__.py
@@ -1,3 +1,3 @@
 from .mouse import buttonOneClick, buttonOneMotion, buttonOneRelease, clearAll, clearLast
-from .collisions import valid_line, valid_wall, valid_waypoint
+from .collisions import valid_line, valid_wall, valid_waypoint, valid__movable_area
 from .level import load, draw_walls, draw_waypoints, hide_waypoints, drawPlayers
--- board/canvas/functions/collisions/__init__.py
+++ board/canvas/functions/collisions/__init__.py
@@ -1,1 +1,1 @@
-from .collisions import valid_line, valid_wall, valid_waypoint
+from .collisions import valid_line, valid_wall, valid_waypoint, valid__movable_area
