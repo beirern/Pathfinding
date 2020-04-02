c86ba4c10aee9554114ed8b3eb81c121a4dbfda6
Nicola Beirer <beirern@uw.edu>
2020-04-01 17:00:32 -0700

    added field for astar solution and index of current path

--- board/canvas/enemy/enemy.py
+++ board/canvas/enemy/enemy.py
@@ -2,8 +2,10 @@ from board.canvas.shape import Shape
 
 
 class Enemy():
-    def __init__(self, shape):
+    def __init__(self, shape, path=None, path_index=0):
         self.shape = shape
+        self.path = path
+        self.path_index = path_index
 
     def __str__(self):
         return str(self.shape)
