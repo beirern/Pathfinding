d9773b76ea7ccccdc9d7d868a57370ec864938fc
Nicola Beirer <beirern@uw.edu>
2020-04-01 17:00:11 -0700

    Added ability for AI to follow solution array path

--- board/canvas/canvas.py
+++ board/canvas/canvas.py
@@ -64,7 +64,7 @@ class Canvas(tk.Canvas):
             '<Double-Button-3>', lambda event: clearAll(self, event))
 
         # Unbind Tab (opposite to mouse binds)
-        self.unbind('<Tab>', self.start_game)
+        self.unbind('<Return>', self.start_game)
         self.unbind('<Up>', self.up)
         self.unbind('<Down>', self.down)
         self.unbind('<Left>', self.left)
@@ -82,7 +82,7 @@ class Canvas(tk.Canvas):
             self.unbind('<Double-Button-3>', self.right_double_click)
 
         # Tab to Start Game (Works opposite of mouse binds)
-        self.start_game = self.bind('<Tab>', self.startGame)
+        self.start_game = self.bind('<Return>', self.startGame)
         self.up = self.bind('<Up>', self.move_up)
         self.down = self.bind('<Down>', self.move_down)
         self.left = self.bind('<Left>', self.move_left)
@@ -93,31 +93,76 @@ class Canvas(tk.Canvas):
 
     def startGame(self, event):
         if self.player != None and self.enemy != None:
-            kdtree = KDTree(self.pixels, self.waypoints)
-            self.astar(kdtree.nearest(self.pixels[self.player.shape.y1 + self.player.shape.height][self.player.shape.x1 + self.player.shape.width]),
-                       kdtree.nearest(self.pixels[self.enemy.shape.y1 + self.enemy.shape.height][self.enemy.shape.x1 + self.enemy.shape.width]))
+            self.kdtree = KDTree(self.pixels, self.waypoints)
+            self.astar_graph = AstarGraph(self.pixels, self.waypoints)
 
-    def astar(self, start, end):
-        graph = AstarGraph(self.pixels, self.waypoints)
-        pathfinder = PathfinderSolver(graph)
+            self.find_path()
+
+    def find_path(self):
+        start = self.kdtree.nearest(
+            self.pixels[self.player.shape.y1][self.player.shape.x1])
+        end = self.kdtree.nearest(
+            self.pixels[self.enemy.shape.y1][self.enemy.shape.x1])
+        pathfinder = PathfinderSolver(self.astar_graph)
         result = pathfinder.findShortestPath(start, end)
 
         if result.outcome == "SOLVED":
             print("DONE")
-            self.print_path(result.solution)
+            solution = result.solution
+            x = self.player.shape.x1
+            y = self.player.shape.y1
+            solution.insert(0, Pixel(x, y, self.pixels[y][x].index, True))
+            self.print_path(solution)
+
+            self.enemy.path = solution
+            self.enemy.path_index = len(solution) - 1
+            self.movement()
         elif result.outcome == "UNSOLVABLE":
             print("Unsolvable!")
         else:
             print("Timed Out!")
 
     def print_path(self, solution):
-        self.create_line(self.player.shape.x1 + self.player.shape.width,
-                         self.player.shape.y1 + self.player.shape.height, solution[0].x, solution[0].y)
         for i in range(len(solution) - 1):
             self.create_line(solution[i].x, solution[i].y,
                              solution[i+1].x, solution[i+1].y)
         self.create_line(solution[len(solution) - 1].x, solution[len(solution) - 1].y,
-                         self.enemy.shape.x1 + self.enemy.shape.width, self.enemy.shape.y1 + self.enemy.shape.height)
+                         self.enemy.shape.x1, self.enemy.shape.y1)
+
+    def movement(self):
+        if self.enemy.path[self.enemy.path_index].x <= self.enemy.shape.x1 + 3 and self.enemy.path[self.enemy.path_index].x >= self.enemy.shape.x1 - 3 and self.enemy.path[self.enemy.path_index].y <= self.enemy.shape.y1 + 3 and self.enemy.path[self.enemy.path_index].y >= self.enemy.shape.y1 - 3:
+            self.enemy.path_index = self.enemy.path_index - 1
+
+        if self.enemy.path_index != -1:
+            rise = self.enemy.path[self.enemy.path_index].y - \
+                self.enemy.shape.y1
+            run = self.enemy.path[self.enemy.path_index].x - \
+                self.enemy.shape.x1
+            if run == 0:
+                slope = rise
+            else:
+                slope = rise // run
+            move_x = 0
+            move_y = 0
+
+            if (run < 0):
+                move_x = -1
+            elif run == 0:
+                move_x = 0
+            else:
+                move_x = 1
+
+            if (rise < 0):
+                move_y = -1 * abs(slope)
+            else:
+                move_y = abs(slope)
+
+            self.move(self.enemy.shape.canvas_id, move_x, move_y)
+            self.enemy.shape.x1 = self.enemy.shape.x1 + move_x
+            self.enemy.shape.y1 = self.enemy.shape.y1 + move_y
+            self.after(100, self.movement)
+        elif self.enemy.path_index == -1:
+            print('Finished moving')
 
     def set_editable(self):
         self.editable = not self.editable
@@ -131,52 +176,36 @@ class Canvas(tk.Canvas):
 
     # Move Up
     def move_up(self, event):
-        self.delete(self.player.shape.canvas_id)
-        player = self.player.shape
         move = valid__movable_area(
-            self, player.x1, player.y1, 'up', player)
+            self, self.player.shape.x1, self.player.shape.y1, 'up', self.player.shape)
         if move != 0:
-            player.y1 = player.y1 - move
-            player.y2 = player.y2 - move
-        new_player = self.create_rectangle(
-            player.x1, player.y1, player.x2, player.y2, width=1, fill="green")
-        self.player.shape.canvas_id = new_player
+            self.move(self.player.shape.canvas_id, 0, -1 * move)
+            self.player.shape.y1 = self.player.shape.y1 - move
+            self.player.shape.y2 = self.player.shape.y2 - move
 
     # Move Down
     def move_down(self, event):
-        self.delete(self.player.shape.canvas_id)
-        player = self.player.shape
         move = valid__movable_area(
-            self, player.x1, player.y2, 'down', player)
+            self, self.player.shape.x1, self.player.shape.y2, 'down', self.player.shape)
         if move != 0:
-            player.y1 = player.y1 + move
-            player.y2 = player.y2 + move
-        new_player = self.create_rectangle(
-            player.x1, player.y1, player.x2, player.y2, width=1, fill="green")
-        self.player.shape.canvas_id = new_player
+            self.move(self.player.shape.canvas_id, 0, move)
+            self.player.shape.y1 = self.player.shape.y1 + move
+            self.player.shape.y2 = self.player.shape.y2 + move
 
     # Move Left
     def move_left(self, event):
-        self.delete(self.player.shape.canvas_id)
-        player = self.player.shape
         move = valid__movable_area(
-            self, player.x1, player.y1, 'left', player)
+            self, self.player.shape.x1, self.player.shape.y1, 'left', self.player.shape)
         if move != 0:
-            player.x1 = player.x1 - move
-            player.x2 = player.x2 - move
-        new_player = self.create_rectangle(
-            player.x1, player.y1, player.x2, player.y2, width=1, fill="green")
-        self.player.shape.canvas_id = new_player
+            self.move(self.player.shape.canvas_id, -1 * move, 0)
+            self.player.shape.x1 = self.player.shape.x1 - move
+            self.player.shape.x2 = self.player.shape.x2 - move
 
     # Move Right
     def move_right(self, event):
-        self.delete(self.player.shape.canvas_id)
-        player = self.player.shape
         move = valid__movable_area(
-            self, player.x2, player.y1, 'right', player)
+            self, self.player.shape.x2, self.player.shape.y1, 'right', self.player.shape)
         if move != 0:
-            player.x1 = player.x1 + move
-            player.x2 = player.x2 + move
-        new_player = self.create_rectangle(
-            player.x1, player.y1, player.x2, player.y2, width=1, fill="green")
-        self.player.shape.canvas_id = new_player
+            self.move(self.player.shape.canvas_id, move, 0)
+            self.player.shape.x1 = self.player.shape.x1 + move
+            self.player.shape.x2 = self.player.shape.x2 + move
